55552032ba21255fdd2dbf56935c2daa023db142
Ganapathi Sivasamy <Ganapathi.X.Sivasamy@aib.ie>
2019-04-24 15:22:59 +0100

    Enable user to add item through plus bar button and update the list once added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Tamil", "Telugu", "Malayalam"]
+    var itemArray = ["Tamil", "Telugu", "Malayalam"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var uiTextField = UITextField()
+        let alert = UIAlertController(title: "Add an Todoey Item", message: "", preferredStyle: .alert)
+        let action =  UIAlertAction(title: "Add", style: .default) { (action) in
+            print("Success")
+            self.itemArray.append(uiTextField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Create an item"
+            uiTextField = textField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
