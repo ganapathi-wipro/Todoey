306916cc1db6bdda325e983abb01e3e48f684235
Ganapathi Sivasamy <Ganapathi.X.Sivasamy@aib.ie>
2019-04-29 12:27:33 +0100

    Convert to MVC

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Tamil", "Telugu", "Malayalam"]
-    
+    var itemArray = [Item]()
+    var defaults = UserDefaults.standard
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let newItem = Item()
+        newItem.title = "Tamil"
+        itemArray.append(newItem)
+        let newItem1 = Item()
+        newItem1.title = "Telugu"
+        itemArray.append(newItem1)
+        
+        if let items = defaults.array(forKey: "RegionalCategoryArray") as? [Item] {
+            itemArray = items
+        }
     }
 
 
@@ -23,7 +34,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add an Todoey Item", message: "", preferredStyle: .alert)
         let action =  UIAlertAction(title: "Add", style: .default) { (action) in
             print("Success")
-            self.itemArray.append(uiTextField.text!)
+            let addItem = Item()
+            addItem.title = uiTextField.text!
+            self.itemArray.append(addItem)
+            self.defaults.set(self.itemArray, forKey: "RegionalCategoryArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (textField) in
@@ -40,19 +54,17 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-       
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
